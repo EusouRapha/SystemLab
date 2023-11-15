2a30bf7217ad6ea261c7db34b4bae1773a5c11f2
Felipe <felipe.reggiane@rethink.dev>
2023-11-14 21:12:05 -0300

    feature: criação das telas

--- systemlab/src/pages/HomeScreen/index.jsx
+++ systemlab/src/pages/HomeScreen/index.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import styles from "./HomeScreen.module.css";
 
 function HomeScreen() {
   return (
-    <>
-      <button>
-        <Link to="/cadastrarlaboratorios">Cadastrar Laboratórios</Link>
-      </button>
-      <button>
-        <Link to="/visualizarlaboratorios">Visualizar Laboratórios</Link>
-      </button>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Olá, gestor</h1>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>
+          <Link to="/cadastrarlaboratorios">Cadastrar Laboratórios</Link>
+        </button>
+        <button className={styles.button}>
+          <Link to="/visualizarlaboratorios">Visualizar Laboratórios</Link>
+        </button>
+      </div>
+    </div>
   );
 }
 
--- systemlab/src/pages/RegisterLabScreen/index.jsx
+++ systemlab/src/pages/RegisterLabScreen/index.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "./RegisterLabScreen.module.css";
+import ImageDropbox from "../components/imageDropbox";
 
 function RegisterLabScreen() {
+  const handleImageUpload = (file) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const imageData = event.target.result;
+      console.log(<img src={imageData} alt="imagem" />);
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <>
+    <div className={styles.container}>
       <button>
         <Link to="/">Home</Link>
       </button>
-      <h1>Registrar Labs</h1>
-    </>
+      <div className={styles.innerContainer}>
+        <h1 className={styles.title}>Cadastrar Laboratório</h1>
+        <p className={styles.dropboxTitle}>Adicione uma foto</p>
+        <ImageDropbox onImageUpload={handleImageUpload} />
+        <div className={styles.inputContainer}>
+          <p className={styles.inputTitle}>Nome:</p>
+          <input type="text" className={styles.input} />
+        </div>
+        <div className={styles.inputContainer}>
+          <p className={styles.inputTitle}>Descrição:</p>
+          <input type="text" className={styles.input} />
+        </div>
+        <button className={styles.button}>Salvar</button>
+      </div>
+    </div>
   );
 }
 
