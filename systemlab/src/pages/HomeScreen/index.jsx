67e43407f18e3a72b7c5f9a5dcf6b8d092f8af7a
EusouRapha <raphaelgw@gmail.com>
2024-10-29 18:33:36 -0300

    feat/TrabXp: aplicando acessibilidade

--- systemlab/src/pages/HomeScreen/index.jsx
+++ systemlab/src/pages/HomeScreen/index.jsx
@@ -1,22 +1,51 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/footer/footer";
 import styles from "./HomeScreen.module.css";
+import { useRef } from "react";
 
 function HomeScreen() {
+  const link1Ref = useRef(null);
+  const link2Ref = useRef(null);
+  const footerLinkRef = useRef(null);
+
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      if (document.activeElement === link1Ref.current) {
+        link2Ref.current.focus();
+      } else {
+        link1Ref.current.focus();
+      }
+    }
+  };
+
   return (
     <>
       <div className={styles.container}>
         <h1 className={styles.title}>Olá, gestor</h1>
         <div className={styles.buttonContainer}>
-          <Link to="/cadastrarlaboratorios" className={styles.link}>
+          <Link
+            to="/cadastrarlaboratorios"
+            className={styles.link}
+            tabIndex="0"
+            ref={link1Ref}
+            onKeyDown={handleKeyDown}
+          >
             Cadastrar Laboratórios
           </Link>
-          <Link to="/visualizarlaboratorios" className={styles.link}>
+          <Link
+            to="/visualizarlaboratorios"
+            className={styles.link}
+            tabIndex="0"
+            ref={link2Ref}
+            onKeyDown={handleKeyDown}
+          >
             Visualizar Laboratórios
           </Link>
         </div>
       </div>
-      <Footer />
+      <Footer footerLinkRef={footerLinkRef} />
     </>
   );
 }
--- systemlab/src/pages/components/footer/footer.jsx
+++ systemlab/src/pages/components/footer/footer.jsx
@@ -2,10 +2,21 @@ import { Link } from "react-router-dom";
 import images from "../../../assets";
 import styles from "./footer.module.css";
 
-const Footer = () => {
+const Footer = ({ footerLinkRef }) => {
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      footerLinkRef.current.click();
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <Link to="https://www.cefetmg.br/">
+      <Link
+        to="https://www.cefetmg.br/"
+        tabIndex="0"
+        ref={footerLinkRef}
+        onKeyDown={handleKeyDown}
+      >
         <img src={images.cefet} alt="cefet" />
       </Link>
     </div>
